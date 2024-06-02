112145fe558e53c2bad99aa3bc9fcbf8b12cf982
narendrayogi914 <narendrayogi914@gmail.com>
2024-06-02 21:56:25 +0530

    New Updated Code

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text',{
+    strings:["Java Developer","Front-End Developer", "System Design", "UI Design"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
