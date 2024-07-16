bd135aa72fdb9de3b2a2c0bb8185231516133983
narendrayogi914 <narendrayogi914@gmail.com>
2024-07-16 22:08:08 +0530

    New Update

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ var typed = new Typed('.text',{
     backDelay:1000,
     loop:true
 });
+
+    (function() {
+        emailjs.init("omnFIBOkMJMBOd-wH1LuM"); // Replace with your EmailJS user ID
+    })();
+
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        // Get form values
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const subject = document.getElementById('subject').value;
+        const message = document.getElementById('message').value;
+
+        // Send email using EmailJS
+        emailjs.send("service_39g46m4", "template_uxlwi9r", {
+            from_name: name,
+            from_email: email,
+            subject: subject,
+            message: message
+        })
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert("Message sent successfully!");
+        }, function(error) {
+            console.log('FAILED...', error);
+            alert("Failed to send the message. Please try again later.");
+        });
+    });
